4db5d130211ffe8413f59538279fc305cb1065dd
Girafa3456 <goncalo.monteiro@ua.pt>
2023-11-26 15:21:39 +0000

    ImageBlend

--- image8bit.c
+++ image8bit.c
@@ -614,7 +614,9 @@ void ImageBlend(Image img1, int x, int y, Image img2, double alpha) { ///
     for (j = x; j < img2->width+x; j++)
     {
       //Cálculo do novo valor do pixel na posição (j, i)
-      uint8 blendPixel = (uint8)(alpha*(ImageGetPixel(img2, j, i) + (1-alpha)*(ImageGetPixel(img1, j, i))));
+      uint8 img1px = ImageGetPixel(img1, j, i);
+      uint8 img2px = ImagegetPixel(img2, j, i);
+      uint8 blendPixel = (uint8)((alpha*img2px) + (1-alpha)*(img1px));
       ImageSetPixel(img1, j, i, blendPixel);
     }
   }
